ab13afa5be495b0dd10cf7a346f71182c63fc137
shivaansharma <113043195+shivaansharma@users.noreply.github.com>
2023-07-28 16:32:36 +0000

    new message

---
+++ pokedex.js
@@ -0,0 +1,34 @@
+
+const pokedex = document.getElementById("pokedex")
+
+function fetchPokemon(){
+const promises =[]
+for(let i =1;i<=150;i++)
+{
+    promises.push(
+fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(name => name.json()))
+    
+    }
+    Promise.all(promises).then(results =>{
+        const pokemon = results.map (data => (
+            {
+                name : data.name,
+                ability :data.abilities.map(able => able.ability.name).join(","),
+                image :  data.sprites['front_default'],
+            }
+
+        ))
+      displayPokemon(pokemon)})
+    .catch()
+    }
+   fetchPokemon()
+    function displayPokemon(pokemon){
+        const pokemonHTMLString = pokemon.map(pokemon=>`
+        <li>
+           <img src = "${pokemon.image}">
+             <h2>${pokemon.name}</h2>
+             <h4>${pokemon.ability}</h2>
+        </li>
+        `)
+      pokedex.innerHTML=pokemonHTMLString
+   }
